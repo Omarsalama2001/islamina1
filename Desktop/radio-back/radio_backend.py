d07fe54f8e46bb9200f5f76793a6686637d4fcd0
Salama <110498127+Omarsalama2001@users.noreply.github.com>
2024-12-02 07:57:05 +0200

    vecel

--- Desktop/radio-back/radio_backend.py
+++ Desktop/radio-back/radio_backend.py
@@ -4,32 +4,28 @@ from models import Radio
 class RadioBackend:
     def __init__(self):
         self.rb = RadioBrowser()
-    
+
     # قائمة بأسماء المحطات
     names: list[str] = [
-        "إذاعة القرآن الكريم من القاهرة",#egypt
-        'Saudi Quran', #saudi arabia
-        '#إذاعة القرآن الكريم',#jordan
-        'Quran Radio راديو القرآن - Kuwait الكويت',#kuwait
-        'Dubai Holy Quran', #dubai
-        # 'Quran Radio Station-Nablus',
-        # 'Radio Quran Karim',
-        # 'قرآن، طعم آفتاب (زنده)'
-        # 'إذاعة طريق السلف'#libya
+      'إذاعة القرآن الكريم (القاهرة)', # Egypt
+        'Saudi Quran',  # Saudi Arabia
+        'Quran Radio راديو القرآن - Kuwait الكويت',  # Kuwait
+        'Dubai Holy Quran',  # Dubai
     ]
 
     def get_radios(self):
         results: list[Radio] = []
-
         # البحث عن كل محطة في القائمة
         for name in self.names:
             radios = self.rb.search(name=name, name_exact=False)
             for radio in radios:
+                print(f"Original URL: {radio['url']}")  # Debug: Original URL from pyradios
+                resolved_url = radio['url']
                 results.append(
                     Radio(
                         country_code=radio['countrycode'], 
                         title=radio['name'], 
-                        audio_url=radio['url_resolved']
+                        audio_url=resolved_url
                     )
                 )
         
